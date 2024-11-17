a1c730a900e1b8f0852ed14f7164876728dcf636
Berkan <berkanncgr@gmail.com>
2024-11-17 12:23:44 +0300

    Complex damage calculations with various attributes

--- Source/Aura/AbilitySystem/ExecCalcs/ExecCalc_Damage.cpp
+++ Source/Aura/AbilitySystem/ExecCalcs/ExecCalc_Damage.cpp
@@ -8,12 +8,14 @@
 struct AuraDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 	
 	AuraDamageStatics()
 	{
 		// We want Armor attribute of the TARGET without snapshot (false)
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet,Armor,Target,false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet,ArmorPenetration,Source,false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet,BlockChance,Target,false);
 	}
 };
@@ -29,19 +31,12 @@ UExecCalc_Damage::UExecCalc_Damage()
 {
 	// ArmorDef came from MACRO In AuraDamageStatics Struct. :)
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorPenetrationDef);
 	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
 {
-	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
-	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
-
-	//const UAuraAttributeSet* TargetAS = Cast<UAuraAttributeSet>(TargetASC->GetAttributeSet(UAuraAttributeSet::StaticClass()));
-	
-	const AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
-	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
-	
 	const FGameplayEffectSpec EffectSpec = ExecutionParams.GetOwningSpec();
 
 	const FGameplayTagContainer* SourceTags = EffectSpec.CapturedSourceTags.GetAggregatedTags();
@@ -53,23 +48,37 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	// Get Damage Set by Caller Magnitude: Damage amount from CT, we captured with correct tag.
 	float Damage = EffectSpec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage,false);
-	
-	float TargetBlockChance = 0; // We captured BlockChance attribute from who we want to give damage.
 
+	// We captured any attribute we want from who we want to give damage (Target).
+	float TargetBlockChance = 0, TargetArmor = 0, SourceArmorPenetration = 0;
+	
 	if(bUseStaticAttributeCapture)
 	{
 		ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef,EvaluateParams,TargetBlockChance);
+		ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef,EvaluateParams,TargetArmor);
+		ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorPenetrationDef,EvaluateParams,SourceArmorPenetration);
 	}
 
 	else
 	{
-		// Actually we can access the attributes this way. Why we tried the capture attribute?
+		// Actually we can access the attributes this way. Why we tried the capture attributes?
+		const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
 		const UAuraAttributeSet* TargetAS = Cast<UAuraAttributeSet>(TargetASC->GetAttributeSet(UAuraAttributeSet::StaticClass()));
 		TargetBlockChance = TargetAS->GetBlockChance();
+		TargetArmor = TargetAS->GetArmor();
+
+		const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+		const UAuraAttributeSet* SourceAS = Cast<UAuraAttributeSet>(SourceASC->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+		SourceArmorPenetration = SourceAS->GetArmorPenetration();
 	}
 
+	// If Block, halve the Damage:
 	const bool bIsBlocked = FMath::RandRange(1,100) < TargetBlockChance;
 	if(bIsBlocked) Damage /= 2;
+
+	// ArmorPenetration ignores percentage of Target's Armor
+	const float EffectiveArmor = TargetArmor *= (100 - (SourceArmorPenetration /4)) / 100;
+	Damage *= (100- EffectiveArmor * 0.3) / 100;
 	
 	const FGameplayModifierEvaluatedData EvaluatedData (UAuraAttributeSet::GetIncomingDamageAttribute(),EGameplayModOp::Additive,Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
